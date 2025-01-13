3ad1f7678c034d2dbf4306d92c139d0a781bb786
Giovane Forlenza <giovane.franciscoforl@gmail.com>
2025-01-13 20:07:17 -0300

    Initial Commit

---
+++ src/app/components/Navbar/Navbar.tsx
@@ -0,0 +1,12 @@
+import React from "react";
+import "../../style/navbar.scss";
+
+function Navbar() {
+  return (
+    <nav className="navbar">
+      <h1 className="page-title">Filmes</h1>
+    </nav>
+  );
+}
+
+export default Navbar;
